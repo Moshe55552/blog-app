08622333cf78ce8ecaf8fe2beddde4b3926d829a
Moshe Oliel <80269537+Moshe55552@users.noreply.github.com>
2024-03-21 10:21:51 +0200

    src/Pages/Register

---
+++ Register.jsx
@@ -0,0 +1,36 @@
+import "./Register.css";
+import { Link } from "react-router-dom";
+
+export default function Register() {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username:</label>
+        <input
+          type="text"
+          placeholder="Enter your username"
+          className="registerInput"
+        />
+        <label>Email:</label>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          className="registerInput"
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          className="registerInput"
+        />
+        <button className="loginButton">Register</button>
+      </form>
+      <button className="registerButton">
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </button>
+    </div>
+  );
+}
