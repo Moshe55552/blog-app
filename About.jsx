4e19dcb18608ad72f089585b9519e78b1fc4207a
Moshe Oliel <80269537+Moshe55552@users.noreply.github.com>
2024-03-21 10:20:41 +0200

    src/Pages/About

---
+++ About.jsx
@@ -0,0 +1,15 @@
+import "./About.css";
+
+export default function About() {
+  return (
+    <div className="about">
+      <div>
+        <h2>About Me:</h2>
+        <p className="par">
+          Im Moshe 26, BSc in Computer Science, looking for my first position. I
+          keep progress and create new things !
+        </p>
+      </div>
+    </div>
+  );
+}
